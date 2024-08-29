7be5c0ac4aeea506fdef3b2561172b5e98686d15
AndresRamirez917 <andresmintic2021@gmail.com>
2024-08-29 17:45:45 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -18,4 +18,67 @@ async function getData(){
     });
 }
 
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+//   });
+
+//   function validarFormulario(e){
+//     e.preventDefault();
+//     let nombre = document.getElementById('nombre').value
+//     if(nombre.length == 0){
+//         alert("ingresa algo sonso")
+//     }return;
+//     this.submit()
+//   }
+
+const btn_enviar = document.getElementById('btn-enviar')
+
+const validar = (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre')
+    const email = document.getElementById('email')
+    const mensaje = document.getElementById('mensaje')
+   
+    if(nombre.value == ""){
+        alert("Escribe algo en el nombre sonso")
+        return false;
+    } 
+
+    if(email.value == ""){
+        alert("Escribe algo en el email pendejo")
+    return false;
+    }
+
+    if(mensaje.value == ""){
+        alert("Debes de dejar un mensaje pendejo")
+        return false;
+    }
+
+    if(!email_valido(email.value)){
+        alert("El email no es válido chinga tu madre")
+        return false;
+    }
+    nombre.value="";
+    email.value="";
+    mensaje.value="";
+    return true;
+}
+const limpiar = (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre')
+    const email = document.getElementById('email')
+    nombre.value="";
+    email.value="";
+} 
+
+
+
+const email_valido = email => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+btn_enviar.addEventListener("click", validar)
+
+
 getData()
